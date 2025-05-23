718ca8498ee3ce008dca0e80d434b80af9727f11
Alice-rgb-rgb <alicesinclair@students.berkeley.net>
2025-05-23 16:17:04 +0000

    Co-authored-by: nathanielanish <nathanielanish@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
+let word;
 let words = ["Apple", "Happy"]
-
+let guesses = 6;
 
 // Marcelo random word generator
 
 function wordGen(){
     let fill = Math.floor(Math.random() * words.length);
-    let word = words[fill];
+    word = words[fill];
     console.log(word);
-    console.log(word.length)
+    console.log(word.length);
 }
 //document.getElementById("submit").addEventListener("click",wordGen);
  
 wordGen();
 
 function checkWord(){
-    for(let i = 0; i < word.length)
+    let guess=document.getElementById("guessWord");
+    for(let i = 0; i < word.length; i++){
+        if (guess[i]==word[i]){
+            makeGreen(guess[i]);
+        }else if (word.includes(guess[i])==true){
+            makeYellow(guess[i]);
+        }else{
+            makeGray(guess[i]);
+        }
+    }
+guesses = guesses - 1;
+if (guesses == 0){
+    
+}
 }
 
 
 document.getElementById("submit").addEventListener("click",checkWord);
+function makeGrid(letters=5){
+    table = document.getElementById("wordGrid");
+    let row = document.createElement("tr");
+    for (let i=0; i<letters; i++){
+        let col = document.createElement("td");
+        col.innerHTML="test";
+        row.appendChild(col);
+    }
+    table.appendChild(row);
+}
+makeGrid();
